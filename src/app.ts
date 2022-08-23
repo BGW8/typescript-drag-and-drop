45d0d5eef1d4301896e7bc2422a6fa5b79f8450a
Additude <additude@Additudes-MacBook-Pro.local>
2022-08-23 16:14:33 +0200

    added submitHandler

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,73 @@
-// Code goes here!
+//! Implement logic in OOP-fashion as to practice using classes
+class ProjectInput {
+	templateElement: HTMLTemplateElement;
+	hostElement: HTMLDivElement;
+	element: HTMLFormElement;
+	titleInputElement: HTMLInputElement;
+	descriptionInputElement: HTMLInputElement;
+	peopleInputElement: HTMLInputElement;
+	//! Seperate selection/Access-logic from calculation-logic
+	//! Place selection-logic and base setup within the constructor
+	constructor() {
+		this.templateElement = document.getElementById(
+			"project-input"
+		) as HTMLTemplateElement;
+
+		this.hostElement = document.getElementById("app") as HTMLDivElement;
+
+		//! import a copy the child-element of the template-element together with
+		//! all of it's children
+		const importedNode = document.importNode(
+			this.templateElement.content,
+			true
+		);
+
+		this.element = importedNode.firstElementChild as HTMLFormElement;
+		//! assign id to apply our styling
+		this.element.id = "user-input";
+
+		this.titleInputElement = this.element.querySelector(
+			"#title"
+		) as HTMLInputElement;
+		this.descriptionInputElement = this.element.querySelector(
+			"#description"
+		) as HTMLInputElement;
+		this.peopleInputElement = this.element.querySelector(
+			"#people"
+		) as HTMLInputElement;
+
+		this.configure();
+		this.attach();
+	}
+	//! Create private methods to contain and handle our caclulation logic
+
+	private submitHandler(event: Event) {
+		event.preventDefault();
+		console.log(this.titleInputElement.value);
+	}
+
+	private configure() {
+		//! All three options below are "valid"/working. Keeping them
+		//! for future reference when it comes to 'this' and how it can
+		//! change depending on context it's being used in.
+		// this.element.addEventListener("submit", function (event: Event) {
+		// 	event.preventDefault();
+		// 	console.log(this);
+		// 	console.log((this.querySelector("#title") as HTMLInputElement).value);
+		// });
+		// this.element.addEventListener("submit", (event: Event) => {
+		// 	event.preventDefault();
+		// 	console.log(this);
+		// 	console.log(this.titleInputElement.value);
+		// });
+
+		this.element.addEventListener("submit", this.submitHandler.bind(this));
+	}
+
+	//!
+	private attach() {
+		this.hostElement.insertAdjacentElement("afterbegin", this.element);
+	}
+}
+
+const prjInput = new ProjectInput();
